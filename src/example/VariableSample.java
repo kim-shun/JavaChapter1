db17e33a5894345ef64aed777bead1f0d937911d
kim-shun <kimura1417080@gmail.com>
2021-06-24 18:18:53 +0900

    変数の宣言

---
+++ src/example/VariableSample.java
@@ -0,0 +1,15 @@
+package example;
+
+public class VariableSample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int num = 10;
+		int num2 = 100;
+		System.out.println(num);
+		num = 20;
+		System.out.println(num);
+		System.out.println(num2);
+	}
+
+}
