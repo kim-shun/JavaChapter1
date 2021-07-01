e036a21c0724b4f60345f2c03dc05a728bbf652a
kim-shun <kimura1417080@gmail.com>
2021-07-01 15:47:53 +0900

    さまざまな型の利用

---
+++ src/example/TypeSample.java
@@ -0,0 +1,20 @@
+package example;
+
+public class TypeSample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int sum = 2.5;
+		System.out.println(sum);
+		
+		double rate = 10.5;
+		System.out.println(rate);
+		
+		boolean isHoliday = true;
+		System.out.println(isHoliday);
+		
+		String title = "いちばんやさしいJava";
+		System.out.println(title);
+	}
+
+}
